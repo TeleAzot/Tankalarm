0e5555621004ae85a83801ee9cb6c7febb7e32bd
TeleAzot <158618141+TeleAzot@users.noreply.github.com>
2026-02-18 15:48:59 +0100

    Experimental WorkManager code.

---
+++ Tankalarm/Platforms/Android/FuelPriceCheckWorker.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using AndroidX.Work;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using System.Net.Http;
+using AndroidApp = Android.App.Application;
+using AndroidContext = Android.Content.Context;
+
+namespace Tankalarm.Platforms.Android
+{
+    public class FuelPriceCheckWorker : Worker
+    {
+        public FuelPriceCheckWorker(Context context, WorkerParameters parameters)
+        : base(context, parameters)
+        {
+        }
+
+        public override Result DoWork()
+        {
+            try
+            {
+                SendNotification("Preis niedrig!");
+
+                return Result.InvokeSuccess();
+            }
+            catch
+            {
+                return Result.InvokeRetry();
+            }
+        }
+
+        private void SendNotification(string message)
+        {
+            var channelId = "priceAlerts";
+            var manager = AndroidApp.Context
+                              .GetSystemService(AndroidContext.NotificationService)
+                              as NotificationManager;
+
+            if (manager.GetNotificationChannel(channelId) == null)
+            {
+                var channel = new NotificationChannel(
+                    channelId, "Preis Alarm", NotificationImportance.Default);
+                manager.CreateNotificationChannel(channel);
+            }
+
+            var builder = new Notification.Builder(AndroidApp.Context, channelId)
+                .SetContentTitle("Preisalarm")
+                .SetContentText(message)
+                .SetSmallIcon(Resource.Drawable.notification_icon_background);
+
+            manager.Notify(1001, builder.Build());
+        }
+    }
+}
--- Tankalarm/Platforms/Android/MainActivity.cs
+++ Tankalarm/Platforms/Android/MainActivity.cs
@@ -1,11 +1,52 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Work;
+using Tankalarm.Platforms.Android;
 
 namespace Tankalarm
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SchedulePriceCheckWorker();
+        }
+
+        /// <summary>
+        /// Check if a worker is running and creates / starts one if not.
+        /// </summary>
+        private void SchedulePriceCheckWorker()
+        {
+            var context = Android.App.Application.Context;
+
+            //only start if user has network
+            var constraints = new Constraints.Builder()
+                .SetRequiredNetworkType(NetworkType.Connected)
+                .Build();
+
+            WorkManager.GetInstance(context)
+                .Enqueue(
+                    OneTimeWorkRequest.Builder
+                        .From<FuelPriceCheckWorker>()
+                        .SetConstraints(constraints)
+                        .Build());
+
+            var request =
+                PeriodicWorkRequest.Builder
+                    .From<FuelPriceCheckWorker>(TimeSpan.FromMinutes(15))
+                    .SetConstraints(constraints)
+                    .Build();
+
+            WorkManager
+                .GetInstance(context)
+                .EnqueueUniquePeriodicWork(
+                    "price_check_worker",                 
+                    ExistingPeriodicWorkPolicy.Keep,
+                    request);
+        }
     }
 }
